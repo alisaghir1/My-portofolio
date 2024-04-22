e5bb18aa25f4425c1977242429a9dfe8dee861c0
alisaghir1 <alisaghir543@gmail.com>
2024-04-22 16:55:40 +0300

    created all the components to start with the the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Services from './components/services/Services.jsx'
+import Portofolio from './components/portofolio/Portofolio.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
+
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portofolio />
+    <Contact/>
+    <Footer />
+    </>
   )
 }
 
